4a02d1d50ce8a2daa5112165b451b472f1eec7fb
Faran Mohammad <ffaranm15@gmail.com>
2020-05-20 15:28:34 +0530

    Project completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'brain_quiz.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain quizBrain=QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -28,6 +32,31 @@ class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper=[
   ];
 
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnswer=quizBrain.getCorrectAnswer();
+    setState(() {
+      if (quizBrain.isFinished() == true) {
+        Alert(
+          context: context,
+          title: 'Finished!',
+          desc: 'You\'ve reached the end of the quiz.',
+        ).show();
+        quizBrain.reset();
+        scoreKeeper = [];
+      }
+      else {
+        if (userPickedAnswer == correctAnswer)
+          scoreKeeper.add(Icon(Icons.check, color: Colors.green,));
+        else
+          scoreKeeper.add(Icon(Icons.close, color: Colors.red,));
+
+        quizBrain.nextQuestion();
+      }
+    });
+  }
+
+  int questionNumber=0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -40,7 +69,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'Faran is the best.',
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -64,11 +93,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                setState(() {
-                  scoreKeeper.add(
-                    Icon(Icons.check, color: Colors.green,),
-                  );
-                });
+                checkAnswer(true);
               },
             ),
           ),
@@ -86,7 +111,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                checkAnswer(false);
               },
             ),
           ),
